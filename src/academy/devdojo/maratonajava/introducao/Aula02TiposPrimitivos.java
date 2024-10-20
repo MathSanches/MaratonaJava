8d84ff1554b7daaabdaa9b2168fb62f02a29d022
Matheus Mariano <m.marianosanches@gmail.com>
2024-10-20 02:18:53 -0300

    Aula 02

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -1,4 +1,17 @@
 package academy.devdojo.maratonajava.introducao;
 
 public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        //int, double, float, char, byte, short, long, boolean
+        int age = 10;
+        long numeroGrande = 10000000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500.0f; // caso você queira especificar que esse numero é realmente de um float, você deve colocar um "f" ou "F" no final
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
+        System.out.println("A idade é "+age);
+    }
 }
